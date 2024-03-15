61dbed5d912165622347a9076b44bd8fc17425dd
jandui <janduineto18@gmail.com>
2024-03-15 14:12:13 -0300

    Chore: Restructured client migrations for declarative data

--- backend/src/database/models/Client.Sequelize.ts
+++ backend/src/database/models/Client.Sequelize.ts
@@ -7,44 +7,50 @@ import {
 } from "sequelize";
 import db from ".";
 
-class SequelizeClient extends Model<InferAttributes<SequelizeClient>,
-    InferCreationAttributes<SequelizeClient>> {
-    public id!: number;
-    public name!: string;
-    public email!: string;
-    public phone!: string;
-    public CPF!: string;
-    public status!: string;
-    }
+class SequelizeClient
+  extends Model<
+    InferAttributes<SequelizeClient>,
+    InferCreationAttributes<SequelizeClient>
+  > {
+  declare id: CreationOptional<number>;
+  declare name: string;
+  declare email: string;
+  declare phone: string;
+  declare CPF: string;
+  declare status: string;
+}
 
 SequelizeClient.init({
-    id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-        allowNull: false,
-    },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    phone: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    CPF: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    status: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  CPF: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  status: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
 }, {
-    sequelize: db,
-    tableName: "clients",
+  sequelize: db,
+  tableName: "clients",
+   timestamps: false,
 });
+
+export default SequelizeClient;
