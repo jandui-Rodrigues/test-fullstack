82cfe1cbab51f9f8a7f5cc1d72ad3b310afa4d43
jandui <janduineto18@gmail.com>
2024-03-17 00:38:47 -0300

    feat: Implements home page, and Create component list cliet

---
+++ frontend/src/Components/ListClient.tsx
@@ -0,0 +1,14 @@
+import DynamicText from "./DynamicText";
+
+function ListClient() {
+  return (
+    <div>
+      <DynamicText
+        titlePage="Listagem de usuários"
+        decription="Escolha o cliente para visualizar os detalhes"
+      />
+    </div>
+  );
+}
+
+export default ListClient;
--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -1,11 +1,19 @@
 import { useNavigate } from "react-router";
+import ListClient from "../Components/ListClient";
+import ButtonBg from "../Components/ButtonBg";
 
 export default function Home() {
   const nav = useNavigate();
   const handle = () => nav("/create");
   return (
-    <div>
-      <button onClick={handle}> Novo Cliente</button>
+    <div className="flex justify-between items-center">
+      <ListClient />
+
+      <ButtonBg
+        handle={handle}
+        className="text-yellow-50 bg-[#E19A37]"
+        nameButton="Novo Cliente"
+      />
     </div>
   );
 }
