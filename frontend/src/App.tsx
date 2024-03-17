fe135f0bc349c476b85d948ea8b29480ae0d372e
jandui <janduineto18@gmail.com>
2024-03-17 00:35:35 -0300

    feat: Implemented a static text header.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
 import { Outlet } from "react-router";
-
+import { User } from "lucide-react";
 function App() {
   return (
     <>
-      <h1>Rodape</h1>
-      <Outlet />
+      <header className="bg-[#333333] flex justify-center">
+        <img src="../img/logo.png" className="w-[80px]" alt="logo uol" />
+      </header>
+      <div className="flex flex-col justify-center  mr-auto ml-auto mt-20 w-4/5">
+        <div className="flex items-center  gap-5">
+          <User size={40} />
+          <h1 className="text-3xl text-gray-800">Painel de Clientes</h1>
+        </div>
+        <Outlet />
+      </div>
     </>
   );
 }
