85291b7cc0f1ab59125a0c40b915ce8e5d248e67
jandui <janduineto18@gmail.com>
2024-03-17 02:20:22 -0300

    feat Add Component Form in aplication.

--- frontend/src/Pages/Form.tsx
+++ frontend/src/Pages/Form.tsx
@@ -1,4 +1,6 @@
 import { useNavigate, useParams } from "react-router";
+import DynamicText from "../Components/DynamicText";
+import ButtonBg from "../Components/ButtonBg";
 
 function Form() {
   const { id } = useParams();
@@ -7,13 +9,29 @@ function Form() {
   const handle = () => {
     return nav("/");
   };
-  
+
   return (
     <div>
-      <h1>Formulario Dinamico</h1>
-
-      <button>{!id ? "Criar" : "Editar"}</button>
-      <button onClick={handle}>Voltar</button>
+      <DynamicText
+        titlePage={!id ? "Novo usuário" : `Editando Cliente`}
+        decription={
+          !id
+            ? "Informe os campos a seguir para criar um novo usuário"
+            : "Altere o campos necessarios para Editar"
+        }
+      />
+      <div className="flex gap-4">
+        <ButtonBg
+          handle={handle}
+          className="text-yellow-50 bg-[#E19A37]"
+          nameButton={!id ? "Criar" : "Editar"}
+        />
+        <ButtonBg
+          handle={handle}
+          className="text-[#E19A37] border-[1px] border-[#E19A37] "
+          nameButton="Voltar"
+        />
+      </div>{" "}
     </div>
   );
 }
